c2a613a54aa38730bdab1bc0f1de9e388989cb01
Angel Afube <angel@zestpayment.com>
2024-07-05 14:57:24 +0100

    add Nav component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,7 +15,7 @@ export default function App({ Component, pageProps }: AppProps) {
           rel="stylesheet"
         />
       </Head>
-      {/* <Nav /> */}
+      <Nav />
       <Component {...pageProps} />
       <Footer />
     </>
